c0ba7498f5de97b2fd2fba45a03939c4170aa8d2
samah-basheer <samahsharafbasheer@gmail.com>
2022-06-18 21:11:29 +0300

    added survey card

--- frontend/src/components/Surveycard.js
+++ frontend/src/components/Surveycard.js
@@ -3,7 +3,74 @@ import './Surveycard.css';
 
 const Surveycard = () => {
     return (
-        <div>test</div>
+        <div className="survey-grid">
+            <div className="single-survey-card">
+                <div className="survey-title">
+                    <p>Survey 1</p>
+                </div>
+                <div className="description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+                <div className="survey-btn">
+                    <div>Fill Survey</div>
+                </div>
+                <div className="spacer"></div>
+                <div className="survey-footer">
+                    <div className="left">
+                        <p className="title">Responses</p>
+                        <p>2500 per question</p>
+                    </div>
+                    <div className="right">
+                        <p className="title">Frequency</p>
+                        <p>One-time</p>
+                    </div>
+                </div>
+            </div>
+            <div className="single-survey-card">
+                <div className="survey-title">
+                    <p>Survey 1</p>
+                </div>
+                <div className="description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+                <div className="survey-btn">
+                    <div>Fill Survey</div>
+                </div>
+                <div className="spacer"></div>
+                <div className="survey-footer">
+                    <div className="left">
+                        <p className="title">Responses</p>
+                        <p>2500 per question</p>
+                    </div>
+                    <div className="right">
+                        <p className="title">Frequency</p>
+                        <p>One-time</p>
+                    </div>
+                </div>
+            </div>
+            <div className="single-survey-card">
+                <div className="survey-title">
+                    <p>Survey 1</p>
+                </div>
+                <div className="description">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+                </div>
+                <div className="survey-btn">
+                    <div>Fill Survey</div>
+                </div>
+                <div className="spacer"></div>
+                <div className="survey-footer">
+                    <div className="left">
+                        <p className="title">Responses</p>
+                        <p>2500 per question</p>
+                    </div>
+                    <div className="right">
+                        <p className="title">Frequency</p>
+                        <p>One-time</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
